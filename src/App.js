f3ee42b2fcf444901da8877b5c5c9b509ce5ebff
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-10-05 18:56:38 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [dataApi, setDataApi] = useState('')
-  const [Ip, setIp] = useState('')
+  const [ip, setip] = useState('38.29.142.180')
+  const [newIp, setNewIp] = useState('')
 
   useEffect(() => {
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=${Ip && '38.29.142.180'}`, {
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&ipAddress=${ip}`, {
       method: 'GET',
       mode: 'cors',
       cache: 'no-cache'
@@ -19,8 +20,8 @@ function App() {
       .then(data => {
         setDataApi(data)
 
-        let current_lat = data.location.lat
-        let current_long = data.location.lng
+        let current_lat = data?.location?.lat ? data.location.lat : 51.505
+        let current_long = data?.location?.lng ? data.location.lng : -0.09
         let current_zoom = 11;
         let center_lat = current_lat;
         let center_long = current_long;
@@ -32,6 +33,7 @@ function App() {
           container._leaflet_id = null;
         }
         // The <div id="map"> must be added to the dom before calling L.map('map')
+
         let map = L.map('map', {
           center: [center_lat, center_long],
           zoom: center_zoom,
@@ -49,14 +51,15 @@ function App() {
           radius: 500
         }).addTo(map);
       })
-
-  }, [Ip])
+    setNewIp('')
+  }, [ip])
   function handleChange(e) {
-    e.preventDefault()
+    setNewIp(e.target.value)
   }
   function handleSubmit(e) {
+    console.log('aaa');
     e.preventDefault()
-    setIp(e.target.value)
+    setip(newIp)
   }
 
   return (
@@ -64,35 +67,38 @@ function App() {
       <div className="App">
         <div className="title">
           <h1>IP Adress Tracker</h1>
-          <form onSubmit={handleSubmit} >
-            <input type="text" value={Ip}
-              placeholder='Search for any IP adress or domain' />
-            <button type="submit" >
-              <svg xmlns="http://www.w3.org/2000/svg" width="10" height="25"><path fill="none" stroke="#FFF" strokeWidth="2" d="M2 1l6 6-6 6" /></svg>
-            </button>
-          </form>
+          <div className="form">
+            <form onSubmit={handleSubmit} >
+              <input type="text"
+                value={newIp}
+                onChange={handleChange}
+                placeholder='Search for any IP adress or domain' />
+              <button type="submit" >
+                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="25"><path fill="none" stroke="#FFF" strokeWidth="2" d="M2 1l6 6-6 6" /></svg>
+              </button>
+            </form>
+          </div>
         </div>
         <div className="info">
           <div className="ip">
             <div className="smallCont">
               IP Address</div>
             {dataApi?.ip}</div>
-            <div class="vl"></div>
+          <div className="vl"></div>
 
           <div className="location"><div className="smallCont">
             Location</div>{dataApi?.location?.city}</div>
-            <div class="vl"></div>
+          <div className="vl"></div>
 
           <div className="timezone"><div className="smallCont">
             Timezone</div>UTC {dataApi?.location?.timezone}</div>
-            <div class="vl"></div>
+          <div className="vl"></div>
           <div className="isp"><div className="smallCont">
             ISP</div>{dataApi?.isp}</div>
         </div>
 
 
         <div id="map" >
-
         </div>
       </div>
 
